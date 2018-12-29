bf85541b09364cc897b8411260c76d6699b9a4e4
Quank <995478874@qq.com>
2018-12-29 21:48:47 +0800

    Update

--- analyzer.py
+++ analyzer.py
@@ -11,19 +11,26 @@ for i in args :
     lstd=inputf.readlines()
     x=0;y=0
     ax=0;ay=0
-    for j in lstd:
-        if j[0]=='#':
+    cnt=0
+    while True:
+        if cnt>=len(lstd):
+            break
+        if lstd[cnt][0]=='P':
+            del lstd[cnt]
             continue
-        if j=='P3\n' :
-            continue
-        elif(len(j.split())==2):
-            ax,ay=j.split()
+        if(len(lstd[cnt].split())==2):
+            ax,ay=lstd[cnt].split()
             ax=int(ax);ay=int(ay)
             outputf.write(str(ax)+' '+str(ay)+'\n')
-        else:
-            outputf.write(str(x)+' '+str(y)+' '+str(int(j))+'\n')
-            x+=1;
-            if(x==ax):
-                x=0
-                y+=1
+            del lstd[cnt]
+        if lstd[cnt][0]=='#':
+            del lstd[cnt]
+            continue
+        cnt+=1
+    for i in range(0,ax):
+        for j in range(0,ay):
+            r=int(lstd[0]);del lstd[0]
+            g=int(lstd[0]);del lstd[0]
+            b=int(lstd[0]);del lstd[0]
+            outputf.write(str(i)+' '+str(j)+' '+str(r)+' '+str(g)+' '+str(b)+'\n')
         
