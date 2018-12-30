41cf690b2b0f30eec61fc65c854ec69249f2f1ff
Quank <995478874@qq.com>
2018-12-30 21:49:04 +0800

    Fixed bug in Issue#1

--- analyzer.py
+++ analyzer.py
@@ -5,32 +5,38 @@ args=sys.argv
 del args[0]
 if (len(args)==0) :
     print("analyzer.py Useage:analyzer.py filename.ppm")
-for i in args :
-    inputf=open(i,mode='r');
-    outputf=open(i+".txt",mode='w')
-    lstd=inputf.readlines()
-    x=0;y=0
-    ax=0;ay=0
-    cnt=0
+intBuffer=[]
+def InitintBuffer(filename):
+    intBuffer.clear()
+    f=open(filename,"r")
     while True:
-        if cnt>=len(lstd):
+        cache=f.readline()
+        if cache=='':
             break
-        if lstd[cnt][0]=='P':
-            del lstd[cnt]
-            continue
-        if(len(lstd[cnt].split())==2):
-            ax,ay=lstd[cnt].split()
-            ax=int(ax);ay=int(ay)
-            outputf.write(str(ax)+' '+str(ay)+'\n')
-            del lstd[cnt]
-        if lstd[cnt][0]=='#':
-            del lstd[cnt]
-            continue
-        cnt+=1
-    for i in range(0,ax):
-        for j in range(0,ay):
-            r=int(lstd[0]);del lstd[0]
-            g=int(lstd[0]);del lstd[0]
-            b=int(lstd[0]);del lstd[0]
-            outputf.write(str(i)+' '+str(j)+' '+str(r)+' '+str(g)+' '+str(b)+'\n')
-        
+        if cache[0]=='P':
+            continue;
+        for i in range(0,len(cache)):
+            if cache[i]=='#':
+                cache=cache[0:i]
+                break
+        intcache=cache.split()
+        for i in intcache:
+            intBuffer.append(int(i))
+    f.close()# Important!
+def Buffer_Getint():
+    p=intBuffer[0]
+    del intBuffer[0]
+    return p
+def Convert(filename):
+    outfile_name='todo_'+filename+'.list'
+    InitintBuffer(filename)
+    outf=open(outfile_name,'w')
+    mx=Buffer_Getint();my=Buffer_Getint();
+    outf.write(str(mx)+' '+str(my)+'\n')
+    Buffer_Getint()
+    for i in range(0,mx):
+        for j in range(0,my):
+            outf.write(str(i)+' '+str(j)+' '+str(Buffer_Getint())+' '+str(Buffer_Getint())+' '+str(Buffer_Getint())+'\n')
+    outf.close()
+for arg in args:
+    Convert(arg)
